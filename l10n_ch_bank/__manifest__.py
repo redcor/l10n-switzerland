8aeab28c2c456c4b310ec7ac969912ef2a5b2e0f
robert <robert@redcor.ch>
2019-10-11 12:58:02 +0200

    [MIG] adapted version in manifest

--- l10n_ch_bank/__manifest__.py
+++ l10n_ch_bank/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Switzerland - Bank list',
-    'version': '11.0.1.0.2',
+    'version': '13.0.1.0.0',
     'author': "Camptocamp, brain-tec AG,Odoo Community Association (OCA)",
     'category': 'Localisation',
     'website': 'http://www.camptocamp.com',
@@ -15,9 +15,6 @@
     # unfortunately it doesn't work with noupdate thus we use a post_init hook
     # 'init': ['data/res.bank.csv'],
     'post_init_hook': 'post_init',
-    'data': [],
-    'images': [],
-    'demo': [],
     'auto_install': False,
     'installable': False,
 }
